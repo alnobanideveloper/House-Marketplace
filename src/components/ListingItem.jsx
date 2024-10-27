2f4661296addc1ba7dae53db1b47fdf6d51c4833
alnobanideveloper <s1.alnobani@gmail.com>
2024-10-27 20:37:46 +0300

    images added to firebase storage and firestore databse

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -10,7 +10,7 @@ export default function ListingItem({ listing, id ,onDelete }) {
             <li className="categoryListing">
                 <Link to={`/category/${listing.type}/${id}`}
                     className="categoryListingLink">
-                    <img src={listing.imageUrls[0]} alt={listing.name}
+                    <img src={listing.imgUrls[0]} alt={listing.name}
                         className="categoryListingImg" />
 
                     <div className="categoryListingDetails">
